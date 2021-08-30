af515cfde1b8c4ff5d7e137fe8dda792ccefb59a
Preranakundu <preranakundu10@hotmail.com>
2021-08-30 22:20:47 +0530

    Update code.py

--- python-with-ds/code.py
+++ python-with-ds/code.py
@@ -6,72 +6,54 @@ class_2 = ['Hilary Mason', 'Carla Gentry','Corinna Cortes']
 
 # Concatenate both the strings
 new_class = class_1 + class_2
-new_class.append('Peter Warden')
-print(new_class)
-new_class.remove('Carla Gentry')
-print(new_class)
 
-courses = {'Math':65 , 'English':70,'History':80, 'French':70, 'Science':60}
-total = sum(courses.values())
-print(total)
-percentage = (total/500 )*100
-print(percentage)
-mathematics = {'Geoffrey Hinton':78 , 'Andrew Ng':95 , 'Sebastian Raschka':65, 'Yoshua Benjio':50 , 'Hilarary Manson':70 , 'Corinna Cortes':66 , 'Peter Warden':75}
-topper = max(mathematics,key = mathematics.get)
-print (topper)
-print(topper.split())
-first_name = (topper.split()[1])
-last_name = (topper.split()[0]) 
-full_name = first_name +' '+ last_name
-
-certificate_name = full_name.upper()
 # Append the list
-print(certificate_name)
+new_class.append('Peter Warden')
 
 # Print updated list
-
+print(new_class)
 
 # Remove the element from the list
+new_class.remove('Carla Gentry')
 
 # Print the list
-
+print(new_class)
 
 
 # Create the Dictionary
-
-
-
-# Slice the dict and stores  the all subjects marks in variable
-
+courses = {'Math':65 , 'English':70,'History':80, 'French':70, 'Science':60}
 
 # Store the all the subject in one variable `Total`
+total = sum(courses.values())
 
 # Print the total
+print(total)
 
 # Insert percentage formula
-
+percentage = (total/500 )*100
 # Print the percentage
-
-
-
+print(percentage)
 
 # Create the Dictionary
  
-
-
+mathematics = {'Geoffrey Hinton':78 , 'Andrew Ng':95 , 'Sebastian Raschka':65, 'Yoshua Benjio':50 , 'Hilarary Manson':70 , 'Corinna Cortes':66 , 'Peter Warden':75}
 
 # Given string
-
+topper = max(mathematics,key = mathematics.get)
+print (topper)
+print(topper.split())
 
 # Create variable first_name 
-
+first_name = (topper.split()[1])
 # Create variable Last_name and store last two element in the list
-
+last_name = (topper.split()[0]) 
 # Concatenate the string
-
+full_name = first_name +' '+ last_name
 # print the full_name
-
+print(full_name)
 # print the name in upper case
+certificate_name = full_name.upper()
+print(certificate_name)
 
 # Code ends here
 
